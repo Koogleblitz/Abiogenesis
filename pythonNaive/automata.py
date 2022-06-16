f1d35ede4242411aab4f720daeb097d7a4c4ff97
richardTobing <rlumb001@ucr.edu>
2022-06-16 00:22:36 -0700

    improved snake ai, from the project 100 submission, it can now grow too, and eats food better, but still needs training

--- pythonNaive/automata.py
+++ pythonNaive/automata.py
@@ -15,17 +15,17 @@ import sys
     # [x] Game of life snek food
     # [x] snek movement
     # [] snek dead
-    # [x] littl brain snek
+    # [x] little brain snek
     # [] big brain snek
+    # [x] elongated snek
     # [] more snek
-    # [] put big brain snek in game of life
     # [] big brain snek have children 
-    # [] Pararell sneks with GPU
+    # [] Pararell multi-threaded sneks with GPU
 
 #[:+:] -- for 16:9 1080p: H=9, W=16, scalar=12, cellsize=10
-# less ideal Cellsize= 12, scalar9, HW6,16,
+# less ideal Cellsize= 12, scalar9, HW:9,16,
 cellSize= 10
-scalar= 7
+scalar= 9
 H= 9*scalar
 W= 16*scalar
 gridDim= (H,W)
@@ -33,18 +33,20 @@ gridTrim= (H-1,W-1)
 resolution= (W*cellSize,H*cellSize)
 CENTER_YX= (resolution[1]//2, resolution[0]//2)
 
-
-tickInterval= 0.01
-spawnRate= 35
-deathRate= 30
-timeSpan= 1023
-chronolog= pygame.time.Clock()
-
-
+automata= True
 wasd= True
 ai= True
 #snake= False
-randomSpawn= True
+randomSpawn= False
+
+
+tickInterval= 0.0001
+spawnRate= 35
+deathRate= 30
+timeSpan= 1023
+snakeLimit= 10
+chronolog= pygame.time.Clock()
+
 
 # https://colordesigner.io/#F34A53-FAE3B4-AAC789-437356-1E4147
 # https://colordesigner.io/#FF6138-FFFF9D-BEEB9F-79BD8F-00A388
@@ -64,7 +66,7 @@ colorRand= [0,0,0]
 # rgb_seedGreen= (210, 0, 255)
 
 
-class Snek:
+class SynchronousSnakeMachine:
     def __init__(self):
         self.x= CENTER_YX[1]
         self.y= CENTER_YX[0]
@@ -73,11 +75,10 @@ class Snek:
         self.bodPos= (self.x-cellSize, self.y)
         self.xStep= 1
         self.yStep= 0
-        self.headSize= (cellSize, cellSize)
-        self.bodSize= (cellSize, cellSize)
+        self.headSize= (cellSize+2, cellSize+2)
+        self.bodSize= ((cellSize), (cellSize))
         self.head= pygame.Rect((self.headPos),(self.headSize))
         self.bodArr= [pygame.Rect((self.bodPos), (self.bodSize))]
-        self.alive= True
     def step(self):
         self.bodArr.append(self.head)
         for i in range(len(self.bodArr)-1):
@@ -89,13 +90,11 @@ class Snek:
             self.xStep= 1
         elif(self.head.x >= W*cellSize-1):
             self.xStep= -1
-
         if(self.head.y <= 1):
             self.yStep= 1
         elif(self.head.y >= H*cellSize-1):
             self.yStep= -1
         self.bodArr.remove(self.head)
-
 
     
 
@@ -105,118 +104,114 @@ def tick(canvas, cellMatrix, cellSize, clk, snek,snake):
     snek.step()
     snek_i= 0
     snekLen= len(snek.bodArr)
-    #pygame.draw.rect(canvas, rgb_red, snek.head)
-    canvas.fill('black')
-    
+    if (clk%73):canvas.fill('black')
 
-    # if (clk%snekLen):
-    #     print("Snek length update: "+ str(snekLen))
-    # else:
-    #     canvas.fill('black')
-    if(snake):
-        for rect in snek.bodArr:
-            pygame.draw.rect(canvas, rgb_lBlue, (rect))
-    
+  
 
+    # for y, x in np.ndindex(gridTrim):
+    if(automata):
+        for y in range(0,gridTrim[0]):
+            #canvas.fill('black')
+            for x in range(0,gridTrim[1]):
+                left= cellMatrix[y][x-1] 
+                right= cellMatrix[y][x+1]
+                top= cellMatrix[y-1][x] 
+                bot= cellMatrix[y+1][x] 
+                topLeft= cellMatrix[y-1][x-1] 
+                topRight= cellMatrix[y-1][x+1]
+                botLeft= cellMatrix[y+1][x-1] 
+                botRight= cellMatrix[y+1][x+1] 
+                neighborhood= left+right+top+bot+topLeft+topRight+botLeft+botRight 
 
-    
-    for y, x in np.ndindex(gridTrim):
-        left= cellMatrix[y][x-1] 
-        right= cellMatrix[y][x+1]
-        top= cellMatrix[y-1][x] 
-        bot= cellMatrix[y+1][x] 
-        topLeft= cellMatrix[y-1][x-1] 
-        topRight= cellMatrix[y-1][x+1]
-        botLeft= cellMatrix[y+1][x-1] 
-        botRight= cellMatrix[y+1][x+1] 
-        neighborhood= left+right+top+bot+topLeft+topRight+botLeft+botRight 
-        rgbState= (
-            rgb_midGreen[0]*cellMatrix[y,x],
-            rgb_midGreen[1]*cellMatrix[y,x],
-            rgb_midGreen[2]*cellMatrix[y,x]    
-        )
-        if cellMatrix[y,x]:
-            rgbState= rgb_darkGreen
-            if 2 <= neighborhood <=3:
-                matrixReloaded[y,x]= 1
-                if(neighborhood==2):
-                    if (left and right) or (top and bot):
-                        rgbState= rgb_red if clk%4 else rgb_orange
-                        # print("foodYX:  " + str(y)+"."+str(x))
-                    elif (topLeft+topRight+botRight+botLeft==2)and(topLeft+botRight==1)and(top+left+bot+right==0):
-                        rgbState= rgb_purple if (clk%3) else rgb_darkBlue
-                        if(ai) and (snake):
-                            if (clk%7):
-                                if (snek.head.x<x*cellSize):
-                                    snek.xStep= 1
-                                elif (snek.head.x>x*cellSize):
-                                    snek.xStep = -1
-                                if (snek.head.y<y*cellSize):
-                                    snek.yStep = 1
-                                elif (snek.head.y>y*cellSize):
-                                    snek.yStep = -1
+                #[:+:] -- Cell Logic ------------------------------------------------------------------------------------------------------/////
+                rgbState=  ('black')
+                if cellMatrix[y,x]:
+                    rgbState= rgb_darkGreen
+                    if 2 <= neighborhood <=3:
+                        matrixReloaded[y,x]= 1
+                        if(neighborhood==2):
+                            if (left and right) or (top and bot):
+                                rgbState=  rgb_orange
+                                # print("foodYX:  " + str(y)+"."+str(x))
+                            elif (topLeft+topRight+botRight+botLeft==2)and(topLeft+botRight==1)and(top+left+bot+right==0):
+                                rgbState= rgb_purple if (clk%9) else rgb_darkBlue
+                            if (abs(x*cellSize-snek.head.x)<=1) and (abs(y*cellSize-snek.head.y)<=10) and (snake):
+                                if(snekLen<snakeLimit):
+                                    snekTangle= pygame.Rect(((snek.head.x, snek.head.y),(cellSize+snekLen//3,cellSize+snekLen//3)))
+                                    snek.bodArr.append(snekTangle)
+                                    snekLen+= 1
+                                    # snek.bodArr.append(pygame.Rect((snek.head.x, snek.head.y),(snek.bodSize)))
+                                rgbState= (0,0,0)
+                                matrixReloaded[y,x]= 0
+                        else:
+                            if(top):
+                                if((topLeft and left) or (topRight and right)):
+                                    rgbState= rgb_darkBlue
+                            elif(bot):
+                                if((botLeft and left)):
+                                    rgbState= rgb_darkBlue
+                                elif (botRight and right):
+                                    rgbState=  rgb_darkGreen
                             else:
-                                randX= random.randint(-1,1)
-                                randY= random.randint(-1,1)
-                                snek.xStep= randX
-                                snek.yStep= randY
-                    if (abs(x*cellSize-snek.head.x)<=1) and (abs(y*cellSize-snek.head.y)<=10) and (snake):
-                        if(snekLen<10):
-                            snek.bodArr.append(pygame.Rect((snek.head.x, snek.head.y),(snek.bodSize)))
-                            snekLen+= 1
-                        rgbState= (0,0,0)
+                                rgbState= rgb_brightGreen
+                    elif neighborhood < 2:
                         matrixReloaded[y,x]= 0
-                else:
-                    if(top):
-                        if((topLeft and left) or (topRight and right)):
-                                rgbState= rgb_darkBlue
-                    elif(bot):
-                            if((botLeft and left)):
-                                rgbState= rgb_darkBlue
-                            elif (botRight and right):
-                                rgbState=  rgb_darkGreen
-                    else:
                         rgbState= rgb_brightGreen
-            elif neighborhood < 2:
-                matrixReloaded[y,x]= 0
-                rgbState= rgb_brightGreen
-            elif neighborhood > 3:
-                matrixReloaded[y,x]= 0
-                rgbState= rgb_midGreen
-            else:
-                rgbState= rgb_brightYellow
-        else:
-            if neighborhood == 3:
-                matrixReloaded[y,x]= 1;
-                rgbState= rgb_darkGreen
-    # [+]:: -- //process rectangles -----------------------------------------------------//
-        cell = pygame.Rect((cellSize*x, cellSize*y),(cellSize-1, cellSize-1))
-        if (x%3):
-            if(snake):
-                for i in range(0,snekLen):
-                    pygame.draw.rect(canvas, rgb_lBlue, snek.bodArr[i])
-        #     for i in range(0,snekLen):
-        #         snekTangle= ((snek.head.x-cellSize*snek.xStep, snek.head.y-cellSize*snek.yStep),(snek.bodSize))
-        #         pygame.draw.rect(canvas, rgb_lBlue, (snekTangle))
-                # snek.bodArr.append(pygame.Rect((snek.head.x, snek.head.y),(snek.bodSize)))
-                # pygame.draw.rect(canvas, rgb_red,snek.bodArr.pop(i))
-    # [+]:: -- \\process rectangles ----------------------------------------------------\\
-        render(canvas, cellMatrix, cell, rgbState, clk)
-    return matrixReloaded, snek
+                    elif neighborhood > 3:
+                        matrixReloaded[y,x]= 0
+                        rgbState= rgb_midGreen
+                    else:
+                        rgbState= rgb_brightYellow
+                else:
+                    if neighborhood == 3:
+                        matrixReloaded[y,x]= 1
+                        rgbState= rgb_darkGreen
+                #-------------------------------------------------------------------------------------------------------------------------////
+
+
+
+                # [+]:: -- //process rectangles + snek brain-----------------------------------------------------//
+                cellTangle = pygame.Rect((cellSize*x, cellSize*y),(cellSize-1, cellSize-1))
+                if(ai) and (snake) and (rgbState==rgb_purple):
+                    if (clk%11):
+                        if (snek.head.x<x*cellSize):
+                            snek.xStep= 1
+                        elif (snek.head.x>x*cellSize):
+                            snek.xStep = -1
+                        if (snek.head.y<y*cellSize):
+                            snek.yStep = 1
+                        elif (snek.head.y>y*cellSize):
+                             snek.yStep = -1
+                    else:
+                        randX= random.randint(-1,1)
+                        randY= random.randint(-1,1)
+                        snek.xStep= randX
+                        snek.yStep= randY
+                    # if (abs(x*cellSize-snek.head.x)<=5) and (abs(y*cellSize-snek.head.y)<=5) and (snake):
+                    # #if(snekLen<10):
+                    #     #snekTangle= pygame.Rect(((snek.head.x, snek.head.y),(snek.bodSize)))
+                    #     snek.bodArr.append(cell)
+                    #     # snek.bodArr.append(pygame.Rect((snek.head.x, snek.head.y),(snek.bodSize)))
+                    #     snekLen+= 1
+                    #     rgbState= (0,0,0)
+                    #     matrixReloaded[y,x]= 0
+                render(canvas, cellMatrix, cellTangle, rgbState, clk,snek,snake,x)
+    return matrixReloaded
         
 
 
 
-def render(canvas,cellMatrix, cell, rgbState,clk):
-    pygame.draw.rect(canvas, rgbState, (cell) )
-    #pygame.draw.rect(canvas, rgb_lBlue, (snekTangle))
+def render(canvas,cellMatrix, cellTangle, rgbState,clk,snek,snake,x):
+    pygame.draw.rect(canvas, rgbState, (cellTangle))
+    if(snake):
+        if(x%3):
+            for i in range(0,len(snek.bodArr)):
+                pygame.draw.rect(canvas, rgb_lBlue, snek.bodArr[i])
+                pygame.draw.rect(canvas, rgb_red, snek.head)
 
-    # for rect in range(0,snekLen):
-    #     pygame.draw.rect(canvas, rgb_beige, snekTangle)
 
     
    
-
 
 
 def main():
@@ -227,12 +222,16 @@ def main():
     clk= 0
     paused= False
     snake= False
-    snek= Snek()
+    snek= SynchronousSnakeMachine()
+    snekMom= SynchronousSnakeMachine()
+    snekDad= SynchronousSnakeMachine()
     numSnek=1
-    # grid.fill('black')
-    # pygame.display.flip()
-    # pygame.display.update()
+    grid.fill('black')
+    pygame.display.flip()
+    pygame.display.update()
     #------------------------------------------//
+
+
     
 
     while True:
@@ -249,21 +248,32 @@ def main():
                     paused= not paused
                     print("[:+:] -- Cell Calculations: "+ str(clk))
                     print("snake size: " + str(len(snek.bodArr)))
-                    print("snakeY.X:  " + str(snek.head.y) +"." + str(snek.head.x))
                     print("Grid Size: " + str(gridDim))
-                    print("snakeYX:  " + str(snek.head.y) +" . " + str(snek.head.x))
+                    print("snakeYX:  " + str(snek.head.y) +" , " + str(snek.head.x))
+                    #print(str(chronolog.get_fps))
+                if event.key == pygame.K_0:
+                    print('Snek0YX:  ' + str(mouseYXRez))
+                    snake = True
+                    # tick(grid,cellMatrix,cellSize,clk, Snek())
+                    # pygame.display.update()
+                if (event.key==pygame.K_e) or (pygame.mouse.get_pressed()[1]):
+                    cellMatrix[mouseY, mouseX:mouseX+2]= 1
+                    cellMatrix[mouseY, mouseX+4:mouseX+7]= 1
+                    mouseX= (W-4) if mouseX >= (W-3) else mouseX
+                    cellMatrix[mouseY-1, mouseX+3]= 1
+                    cellMatrix[mouseY-2, mouseX+1]= 1
                 if(wasd):
                     if event.key == pygame.K_w:
                         snek.yStep= -1 
-                        snek.xStep= 0  
+                        #snek.xStep= 0  
                     if event.key == pygame.K_a:
-                        snek.yStep= 0
+                        #snek.yStep= 0
                         snek.xStep= -1 
                     if event.key == pygame.K_s:
                         snek.yStep= 1
-                        snek.xStep= 0 
+                        #snek.xStep= 0 
                     if event.key == pygame.K_d:
-                        snek.yStep= 0
+                        #snek.yStep= 0
                         snek.xStep= 1 
             if pygame.mouse.get_pressed()[0]:
                 print('left clickYX:  ' + str(mouseYXRez) )
@@ -272,12 +282,14 @@ def main():
                 tick(grid,cellMatrix,cellSize,clk,snek, snake)
                 pygame.display.update()
             if pygame.mouse.get_pressed()[2]:
-                print('right clickYX:  ' + str(mouseYXRez) )
-                snake = True
-                # tick(grid,cellMatrix,cellSize,clk, Snek())
-                # pygame.display.update()
-            if pygame.mouse.get_pressed()[1]:
-                print('middle click:  ' + str(mouseYXRez) )
+                print('Right clickYX:  ' + str(mouseYXRez) )
+                mouseX= (W-2) if mouseX >= (W-1) else mouseX
+                cellMatrix[mouseY][mouseX-1]= 1 
+                cellMatrix[mouseY][mouseX+1]= 1
+                cellMatrix[mouseY-1][mouseX]= 1 
+                cellMatrix[mouseY+1][mouseX]= 1 
+            if (pygame.mouse.get_pressed()[1]):
+                print('middle clickYX:  ' + str(mouseYXRez) )
                 cellMatrix[mouseY, mouseX:mouseX+2]= 1
                 cellMatrix[mouseY, mouseX+4:mouseX+7]= 1
                 mouseX= (W-4) if mouseX >= (W-3) else mouseX
@@ -311,21 +323,17 @@ def main():
 
 
 
-        #---[+]:: Update Functions-------------------------------//
+        #---[+]:: Update Functions + Clock Stuff-------------------------------//
         if not paused:
+            cellMatrix = tick(grid, cellMatrix, cellSize,clk, snek, snake)
             clk = 0 if clk >= timeSpan else clk+1 
-            cellMatrix = tick(grid, cellMatrix, cellSize,clk, snek, snake)[0]
-            # snek= tick(grid, cellMatrix, cellSize,clk, snek)[1]
-            # snekTick(grid,cellMatrix, snek)
-            
             pygame.display.update()
+            chronolog.tick
             clk+=1
-            #if(clk%10):print("Cell Calculations: "+ str(clk))
+            #clock.tick(10)
+            time.sleep(tickInterval)
         #---[+]:: \\Update Functions----------------------------//
 
-
-        time.sleep(tickInterval)
-        #clock.tick(10)
 if __name__ == '__main__':
     main()
 
